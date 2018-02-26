2d33a7305986862e6b6572ff98bdfa1c6a8e8142
jiajihe <83583245@qq.com>
2018-02-26 14:49:36 +0800

    [jiaji] add marriage date countdown

--- container/home.js
+++ container/home.js
@@ -7,6 +7,7 @@ import homeFocusIcon from '../images/home-focus.png';
 
 const datingDate = new Date('2015-08-08');
 const engageDate = new Date('2017-08-12');
+const marriedDate = new Date('2018-02-14');
 
 const styles = StyleSheet.create({
   container: {
@@ -26,6 +27,12 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
   },
+  engagedDateText: {
+    fontSize: 16,
+  },
+  marriedDateText: {
+    fontSize: 20,
+  }
 });
 
 export default class Home extends React.Component {
@@ -33,6 +40,7 @@ export default class Home extends React.Component {
     const now = Date.now();
     const dayFromDating = calculateDaysBetween(datingDate, now);
     const dayFromEngage = calculateDaysBetween(engageDate, now);
+    const dayFromMarriage = calculateDaysBetween(marriedDate, now);
     return (
       <View style={styles.container}>
         <View style={styles.titleContainer}>
@@ -40,7 +48,8 @@ export default class Home extends React.Component {
         </View>
         <View style={styles.contentContainer}>
           <Text>这是我们在一起的第{dayFromDating}天.</Text>
-          <Text>这是我们订婚的第{dayFromEngage}天.</Text>
+          <Text style={styles.engagedDateText}>这是我们订婚的第{dayFromEngage}天.</Text>
+          <Text style={styles.marriedDateText}>这是我们结婚的第{dayFromMarriage}天.</Text>
         </View>
       </View>
     );
